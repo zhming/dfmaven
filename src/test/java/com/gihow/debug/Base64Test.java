096fe9efb5eb173564f60831d3ac0230bb999910
zhming <zhming.m@gmail.com>
2016-05-13 17:31:54 +0800

    eeee

---
+++ src/test/java/com/gihow/debug/Base64Test.java
@@ -0,0 +1,23 @@
+package com.gihow.debug;
+
+import com.gihow.dfc.TrustedAuthenticatorUtils;
+import com.gihow.util.Base64;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Bourne Qian
+ * Date: 16-5-13
+ * Time: 下午4:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class Base64Test {
+    @Test
+    public void descTest(){
+        String ss = "gihow1qaz";
+            System.out.println("aaaaaaaaaaaaaaaaa : "  + Base64.encode(ss));
+    }
+}
