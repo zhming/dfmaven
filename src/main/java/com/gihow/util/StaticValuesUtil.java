36e31d658e59ac4697624efd395a43d8a15319a0
zhming <zhming.m@gmail.com>
2016-05-17 17:27:59 +0800

    eeee

---
+++ src/main/java/com/gihow/util/StaticValuesUtil.java
@@ -0,0 +1,15 @@
+package com.gihow.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Bourne Qian
+ * Date: 16-5-16
+ * Time: 下午3:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class StaticValuesUtil {
+    public static String DOCBASE = "GihowR";
+    public static String GLOBAL_USERNAME = "Administrator";
+    public static String GLOBAL_PASSWORD = "gihow1qaz";
+
+}
